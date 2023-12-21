077f501411d88d68fc4f829f06ac16d4dbc478e0
gl1tchInc <glitch5e3@gmail.com>
2023-12-21 15:03:43 +0300

    Add project files.

---
+++ DB_LR_7-8/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace DB_LR_7_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.tableList = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.queryList = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToOrderColumns = true;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(12, 12);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowHeadersWidth = 51;
+            this.dataGrid.RowTemplate.Height = 24;
+            this.dataGrid.Size = new System.Drawing.Size(767, 418);
+            this.dataGrid.TabIndex = 0;
+            this.dataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(785, 162);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(125, 50);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "SAVE CHANGES";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.onSaveChangesClick);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(785, 218);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(125, 50);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "DELETE";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.onDeleteClick);
+            // 
+            // tableList
+            // 
+            this.tableList.FormattingEnabled = true;
+            this.tableList.ItemHeight = 16;
+            this.tableList.Location = new System.Drawing.Point(785, 40);
+            this.tableList.Name = "tableList";
+            this.tableList.Size = new System.Drawing.Size(155, 116);
+            this.tableList.TabIndex = 3;
+            this.tableList.SelectedIndexChanged += new System.EventHandler(this.SelectTableFromTableList);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(785, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "TABLES";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // queryList
+            // 
+            this.queryList.FormattingEnabled = true;
+            this.queryList.ItemHeight = 16;
+            this.queryList.Location = new System.Drawing.Point(972, 40);
+            this.queryList.Name = "queryList";
+            this.queryList.Size = new System.Drawing.Size(155, 116);
+            this.queryList.TabIndex = 5;
+            this.queryList.SelectedIndexChanged += new System.EventHandler(this.SelectQueryFromQueriesList);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(972, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "QUERIES";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 514);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.queryList);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tableList);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.dataGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.onForm1Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.ListBox tableList;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox queryList;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ DB_LR_7-8/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Collections.Generic;
+
+namespace DB_LR_7_8
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Переменная, предназначенная для соединения с БД, разработанной в предыдущих ЛР
+        /// </summary>
+        private OleDbConnection connection = null;
+        /// <summary>
+        /// Переменная, предназначенная для хранения данных, выгруженных из БД
+        /// </summary>
+        private DataSet dataSet;
+        /// <summary>
+        /// Переменная, предназначенная для извлечения данных из БД в датасет
+        /// </summary>
+        private OleDbDataAdapter dataAdapter;
+        /// <summary>
+        /// Переменная, предназначенная для автоматической генерации операций над датасетом
+        /// </summary>
+        private OleDbCommandBuilder commandBuilder;
+        /// <summary>
+        /// Множество выборочных запросов, которые надо выгрузить из БД
+        /// </summary>
+        private HashSet<string> queriesSet = new HashSet<string>(
+            new string[] { "_select1", "_select2", "_select3", "_where1", "_where2", "_where4" });
+        /// <summary>
+        /// Множество запросов на изменение данных, которые надо выгрузить из БД
+        /// </summary>
+        private HashSet<string> proceduresSet = new HashSet<string>(
+            new string[] { "6_insertAutobus", "6_deleteAutobus", "6_update1"});
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Метод, вызываемый при загрузке Form1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void onForm1Load(object sender, EventArgs e)
+        {
+            connection = new OleDbConnection(
+                ConfigurationManager.ConnectionStrings["DataBase_LR"].ConnectionString);
+
+            connection.Open();
+
+            Console.WriteLine(connection.State == ConnectionState.Open ?
+                "Подключение установлено" : "Не удалось установить подключение");
+
+            FillTableList();
+            FillQueriesList();
+        }
+        /// <summary>
+        /// Метод, отвечающий за отображение выбранной таблицы на экране
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectTableFromTableList(object sender, EventArgs e)
+        {
+            try
+            {
+                string selectedTable = tableList.SelectedItem.ToString();
+                string query = $"SELECT * FROM {selectedTable}";
+                OleDbCommand command = new OleDbCommand(query, connection);
+
+                dataAdapter = new OleDbDataAdapter(command);
+                dataSet = new DataSet();
+
+                dataAdapter.Fill(dataSet);
+
+                dataGrid.DataSource = dataSet.Tables[0];
+
+                commandBuilder = new OleDbCommandBuilder(dataAdapter);
+                dataAdapter.UpdateCommand = commandBuilder.GetUpdateCommand();
+                dataAdapter.InsertCommand = commandBuilder.GetInsertCommand();
+                dataAdapter.DeleteCommand = commandBuilder.GetDeleteCommand();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading data: " + ex.Message, "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+        /// <summary>
+        /// Заполнить список таблиц
+        /// </summary>
+        private void FillTableList()
+        {
+            try
+            {
+                DataTable schemaTable = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, 
+                    new object[] { null, null, null, "TABLE" });
+                foreach (DataRow row in schemaTable.Rows)
+                {
+                    string tableName = row["TABLE_NAME"].ToString();
+                    tableList.Items.Add(tableName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading table names: " + ex.Message, "Error", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        /// <summary>
+        /// Метод отвечающий за сохранение внесённых изменений по нажатию кнопки saveButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void onSaveChangesClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataAdapter is null || dataSet is null) throw new Exception("No loaded table to save");
+
+                dataAdapter.Update(dataSet.Tables[0]);
+
+                MessageBox.Show("Data saved successfully.", "Success", MessageBoxButtons.OK, 
+                    MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving data: " + ex.Message, "Error", MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
+            }
+        }
+        /// <summary>
+        /// Метод, отвечающий за удаление строчки из таблицы по нажатию кнопки deleteButton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void onDeleteClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGrid.SelectedRows.Count > 0)
+                {
+                    int selectedIndex = dataGrid.SelectedRows[0].Index;
+                    dataGrid.Rows.RemoveAt(selectedIndex);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error deleting data: " + ex.Message, "Error", MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
+            }
+        }
+        /// <summary>
+        /// Метод, отвечающий за выполнение запроса находящегося в queryList
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectQueryFromQueriesList(object sender, EventArgs e)
+        {
+            try
+            {
+                string selected = queryList.SelectedItem.ToString();
+
+                if( queriesSet.Contains(selected))
+                {
+                    string query = $"SELECT * FROM {selected}";
+                    OleDbCommand command = new OleDbCommand(query, connection);
+
+                    dataAdapter = new OleDbDataAdapter(command);
+                    dataSet = new DataSet();
+
+                    dataAdapter.Fill(dataSet);
+
+                    dataGrid.DataSource = dataSet.Tables[0];
+                }
+                else if (proceduresSet.Contains(selected))
+                {
+                    try
+                    {
+                        OleDbCommand command = new OleDbCommand(selected, connection);
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.ExecuteNonQuery();
+
+                        MessageBox.Show("Procedure executed successfully.", "Success", 
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show("Error executing procedure: " + ex.Message, "Error", 
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                /*commandBuilder = new OleDbCommandBuilder(dataAdapter);
+                dataAdapter.UpdateCommand = commandBuilder.GetUpdateCommand();
+                dataAdapter.InsertCommand = commandBuilder.GetInsertCommand();
+                dataAdapter.DeleteCommand = commandBuilder.GetDeleteCommand();*/
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading data: " + ex.Message, "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+        /// <summary>
+        /// Заполнить список запросов
+        /// </summary>
+        private void FillQueriesList()
+        {
+            try
+            {
+                DataTable schemaTable = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables,
+                    new object[] { null, null, null, "VIEW" });
+                foreach (DataRow row in schemaTable.Rows)
+                {
+                    string tableName = row["TABLE_NAME"].ToString();
+                    if(queriesSet.Contains(tableName))
+                        queryList.Items.Add(tableName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading table names: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            try
+            {
+                DataTable schemaTable = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Procedures,
+                    new object[] { null, null, null, null });
+
+                foreach (DataRow row in schemaTable.Rows)
+                {
+                    string procedureName = row["PROCEDURE_NAME"].ToString();
+                    if(proceduresSet.Contains(procedureName))
+                        queryList.Items.Add(procedureName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading stored procedure names: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void contextMenuStrip1_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
